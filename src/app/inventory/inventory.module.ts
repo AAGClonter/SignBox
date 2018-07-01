36585ffab58b24ea637e297f9f9896a6b53721c3
Andy Alcantara <andyalcantara745@yahoo.com>
2018-07-01 10:19:40 -0400

    Added prebuilt styles

--- src/app/inventory/inventory.module.ts
+++ src/app/inventory/inventory.module.ts
@@ -14,6 +14,7 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MatGridListModule } from '@angular/material/grid-list';
 
 @NgModule({
     declarations: [
@@ -29,7 +30,8 @@ import { MatCardModule } from '@angular/material/card';
         MatFormFieldModule,
         MatInputModule,
         MatButtonModule,
-        MatCardModule
+        MatCardModule,
+        MatGridListModule
     ],
     providers: [
         InventoryService
